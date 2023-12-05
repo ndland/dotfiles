074ef9f2484dcb5407f336db99b0bd122e371399
Nick Land <land.d.nicholas@gmail.com>
2023-12-05 10:30:54 -0500

    fix(telescope): update telescope
    * This updates Telescope version
    I had an issue where Telescope wouldn't work at all, this fixes that.

--- .config/nvim/lua/ndland/packer.lua
+++ .config/nvim/lua/ndland/packer.lua
@@ -9,7 +9,7 @@ return require("packer").startup(function(use)
 
 	use({
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
+		tag = "0.1.5",
 		requires = {
 			{ "nvim-lua/plenary.nvim" },
 			{ "nvim-telescope/telescope-file-browser.nvim" },
