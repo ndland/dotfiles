77577e3153026d370d3e5d33f8d50812c0cdef80
Nick Land <land.d.nicholas@gmail.com>
2024-05-09 18:26:25 -0400

    delete: untrack history.el

---
+++ .emacs.d/transient/history.el
@@ -0,0 +1,10 @@
+((magit-commit nil
+	       ("--reuse-message=ORIG_HEAD"))
+ (magit-diff
+  ("--no-ext-diff" "--stat"))
+ (magit-dispatch nil)
+ (magit-fetch nil)
+ (magit-pull nil
+	     ("--ff-only")
+	     ("--rebase"))
+ (magit-push nil))
