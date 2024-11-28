5f59fa3ed16f111300f7a71c190c84e8c0721e96
Nick Land <land.d.nicholas@gmail.com>
2024-11-27 19:59:54 -0500

    feat(Emacs): add lsp-tailwindcss
    * this commit also adds icons to completion menu in Emacs.

--- emacs/.doom.d/packages.el
+++ emacs/.doom.d/packages.el
@@ -11,6 +11,7 @@
 (package! org-roam-ui)
 (package! jest)
 (package! exec-path-from-shell)
+(package! lsp-tailwindcss :recipe (:host github :repo "merrickluo/lsp-tailwindcss"))
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
