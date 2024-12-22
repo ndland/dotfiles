6b16a1e3d277fcda32d96e30e9d1434461b99a03
Nick Land <land.d.nicholas@gmail.com>
2024-12-21 19:42:29 -0500

    ♻️ refactor(doom): move doom to new directory
    I want to keep my doom emacs config, but I am doing a roll your own
    solution as well that I want to keep. It makes more sense for doom to
    be in the doom top-level directory.

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "JetBrainsMono" :size 13 :weight 'regular)
-     doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+(setq doom-font (font-spec :family "Iosevka" :size 14 :weight 'regular)
+     doom-variable-pitch-font (font-spec :family "Iosevka Etoile" :size 14))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-tokyo-night)
+(setq doom-theme 'doom-dracula)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
