59e7e95002f7558414768749a7d8f313e3ce43df
Nick Land <land.d.nicholas@gmail.com>
2024-12-22 14:13:13 -0500

    ✨ feat(Emacs): add line numbers * this applies to all programming modes, thus org-mode and plain text will not have line numbers, as well as mini-buffers and the like.

--- emacs/.emacs.d/config/core.el
+++ emacs/.emacs.d/config/core.el
@@ -1,3 +1,6 @@
 ;; Basic settings
 (setq inhibit-startup-message t)
 (setq backup-directory-alist `(("." . "~/.emacs.d/backups")))
+
+(setq display-line-numbers t)
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
