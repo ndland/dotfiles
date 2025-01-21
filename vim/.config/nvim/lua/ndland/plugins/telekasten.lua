ebaf52b4fc0592b67be6b0cb7e4da3039e424928
Nick Land <land.d.nicholas@gmail.com>
2025-01-21 10:29:38 -0500

    fix(nvim): update telekasten file paths
    Add a conditional to telekasten based on which system I'm on. The paths are not the same, and as such, they need to be checked so that I'm using the correct path.

--- vim/.config/nvim/lua/ndland/plugins/telekasten.lua
+++ vim/.config/nvim/lua/ndland/plugins/telekasten.lua
@@ -4,12 +4,21 @@ return {
   config = function()
     local telekasten = require("telekasten")
 
+    local hostname = vim.fn.hostname()
+    local zettle_home
+
+    if hostname == "GMMACANCNXYVH42" then
+      zettle_home = vim.fn.expand("~/code/personal/github.com/ndland/zettle/")
+    else
+      zettle_home = vim.fn.expand("~/code/github.com/ndland/zettle/")
+    end
+
     telekasten.setup({
-      home = vim.fn.expand("~/code/github.com/ndland/zettle"),
-      dailies = vim.fn.expand("~/code/github.com/ndland/zettle/daily/"), -- path to daily notes
-      weeklies = vim.fn.expand("~/code/github.com/ndland/zettle/weekly/"), -- path to weekly notes
-      templates = vim.fn.expand("~/code/github.com/ndland/zettle/templates/"), -- path to templates
-      template_new_daily = vim.fn.expand("~/code/github.com/ndland/zettle/templates/daily.md"), -- path to new daily template
+      home = vim.fn.expand(zettle_home), -- path to zettlekasten
+      dailies = vim.fn.expand(zettle_home .. "/daily/"), -- path to daily notes
+      weeklies = vim.fn.expand(zettle_home .. "/weekly/"), -- path to weekly notes
+      templates = vim.fn.expand(zettle_home .. "/templates/"), -- path to templates
+      template_new_daily = vim.fn.expand(zettle_home .. "/templates/daily.md"), -- path to new daily template
       extension = ".md",
       image_subdir = "img",
     })
@@ -18,6 +27,13 @@ return {
       pattern = "telekasten",
       callback = function()
         vim.bo.filetype = "markdown"
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function()
+        vim.cmd("setlocal spell")
+        vim.opt_local.conceallevel = 2
       end,
     })
   end,
