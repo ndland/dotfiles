4de0a6db99c883a70085f2b67fd66c4541c5d337
Nick Land <land.d.nicholas@gmail.com>
2025-01-22 15:48:05 -0500

    fix: update dots

--- vim/.config/nvim/lua/ndland/plugins/which-key.lua
+++ vim/.config/nvim/lua/ndland/plugins/which-key.lua
@@ -32,8 +32,21 @@ return {
       { "<leader>etf", "<cmd>tabnew %<CR>", desc = "Open current buffer in new tab" },
 
       { "<leader>n", group = "notes" },
-      { "<leader>np", "<cmd>Telekasten panel<cr>", desc = "Telekasten panel" },
-      { "<leader>ns", "<cmd>Telekasten search_notes<cr>", desc = "Search Notes" },
+      { "<leader>ns", "<cmd>ZkNotes<cr>", desc = "ZK Notes" },
+      { "<leader>nd", "<cmd>ZkNew { dir = 'daily', date = 'today' }<cr>", desc = "ZK Daily Note" },
+      {
+        "<leader>nn",
+        function()
+          vim.ui.input({ prompt = "Enter note title: " }, function(input)
+            if input then
+              require("zk").new({ title = input })
+            end
+          end)
+        end,
+        desc = "ZK New",
+      },
+      -- { "<leader>np", "<cmd>Telekasten panel<cr>", desc = "Telekasten panel" },
+      -- { "<leader>ns", "<cmd>Telekasten search_notes<cr>", desc = "Search Notes" },
 
       { "<leader>s", group = "splits" },
       { "<leader>sv", "<C-w>v", desc = "Split window vertically" },
@@ -41,6 +54,13 @@ return {
       { "<leader>se", "<C-w>=", desc = "Make splits equal size" },
       { "<leader>sx", "<cmd>close<CR>", desc = "Close current split" },
       { "<leader>sm", "<cmd>MaximizerToggle<CR>", desc = "Close current split" },
+
+      { "<leader>x", group = "Trouble" },
+      { "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open trouble workspace diagnostics" },
+      { "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Open trouble document diagnostics" },
+      { "<leader>xq", "<cmd>Trouble quickfix toggle<CR>", desc = "Open trouble quickfix list" },
+      { "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
+      { "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
     })
   end,
   opts = {},
