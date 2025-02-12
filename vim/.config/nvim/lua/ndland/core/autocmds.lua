7fa8c82c9a3440ee7563c72e3a5cabc42cc55f7b
Nick Land <land.d.nicholas@gmail.com>
2025-02-12 13:51:40 -0500

    ✨ feat(nvim): update zk/nvim config

--- vim/.config/nvim/lua/ndland/core/autocmds.lua
+++ vim/.config/nvim/lua/ndland/core/autocmds.lua
@@ -22,8 +22,35 @@ vim.api.nvim_create_autocmd("FileType", {
     -- Additional settings
     vim.opt_local.spell = true -- Enable spell checking
     vim.opt_local.conceallevel = 2 -- Enable conceal for syntax
+
+    -- Indentation settings for Markdown
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true -- Use spaces instead of tabs for indentation
+
+    -- Ensure proper indentation for lists
+    vim.opt_local.formatoptions:append("n") -- Automatically indent new list items
   end,
 })
+
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "markdown",
+--   callback = function()
+--     -- Enable soft wrapping
+--     vim.opt_local.wrap = true
+--     vim.opt_local.linebreak = true -- Break soft-wrapped lines at word boundaries
+--
+--     -- Configure hard wrapping for new text
+--     vim.opt_local.textwidth = 80 -- Set maximum width for new lines
+--     vim.opt_local.formatoptions:append("t") -- Auto-wrap text as you type
+--     vim.opt_local.formatoptions:remove("c") -- Avoid comment wrapping for markdown
+--
+--     -- Additional settings
+--     vim.opt_local.spell = true -- Enable spell checking
+--     vim.opt_local.conceallevel = 2 -- Enable conceal for syntax
+--   end,
+-- })
 
 -- Reformat existing lines manually or on save
 vim.api.nvim_create_autocmd("BufWritePre", {
--- vim/.config/nvim/lua/ndland/plugins/zk.lua
+++ vim/.config/nvim/lua/ndland/plugins/zk.lua
@@ -2,7 +2,26 @@ return {
   "zk-org/zk-nvim",
   config = function()
     require("zk").setup({
-      picker = "telescope",
+      -- Can be "telescope", "fzf", "fzf_lua", "minipick", "snacks_picker",
+      -- or select" (`vim.ui.select`). It's recommended to use "telescope",
+      -- "fzf", "fzf_lua", "minipick", or "snacks_picker".
+      picker = "snacks_picker",
+
+      lsp = {
+        -- `config` is passed to `vim.lsp.start_client(config)`
+        config = {
+          cmd = { "zk", "lsp" },
+          name = "zk",
+          -- on_attach = ...
+          -- etc, see `:h vim.lsp.start_client()`
+        },
+
+        -- automatically attach buffers in a zk notebook that match the given filetypes
+        auto_attach = {
+          enabled = true,
+          filetypes = { "markdown" },
+        },
+      },
     })
   end,
 }
