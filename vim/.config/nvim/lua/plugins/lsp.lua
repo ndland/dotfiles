4b2e0fb9866ebeec0db6ab847927e27452d972a1
Nick Land <land.d.nicholas@gmail.com>
2025-07-17 08:53:33 -0400

    docs(nvim): remove comments from plugins

--- vim/.config/nvim/lua/plugins/lsp.lua
+++ vim/.config/nvim/lua/plugins/lsp.lua
@@ -2,7 +2,6 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile", "VeryLazy" },
 	keys = {
-		-- Top Pickers & Explorer
 		{
 			"<leader>ca",
 			function()
@@ -12,7 +11,6 @@ return {
 		},
 	},
 	config = function()
-		local lspconfig = require("lspconfig")
 		local configs = require("lspconfig/configs")
 
 		configs.zk = {
@@ -26,23 +24,13 @@ return {
 			},
 		}
 
-		lspconfig.zk.setup({
-			on_attach = function(client, buffer)
-				-- Add keybindings here, see https://github.com/neovim/nvim-lspconfig#keybindings-and-completion
-			end,
-		})
-
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 			callback = function(ev)
-				-- Buffer local mappings.
-				-- See `:help vim.lsp.*` for documentation on any of the below functions
 				local opts = { buffer = ev.buf, silent = true }
 			end,
 		})
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
 		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
