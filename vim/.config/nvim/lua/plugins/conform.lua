543bdd636c1b8cde6e9b505c328dfe7df3b8efb3
Nick Land <land.d.nicholas@gmail.com>
2025-07-16 20:05:37 -0400

    :sparkles: feat(nvim): update formatting and add keybinds for zk

--- vim/.config/nvim/lua/plugins/conform.lua
+++ vim/.config/nvim/lua/plugins/conform.lua
@@ -4,12 +4,13 @@ return {
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
+				markdown = { "markdownlint" },
 				lua = { "stylua" },
 			},
 			format_on_save = {
-				-- These options will be passed to conform.format()
-				timeout_ms = 500,
-				lsp_format = "fallback",
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
 			},
 		})
 	end,
--- vim/.config/nvim/lua/plugins/zk-nvim.lua
+++ vim/.config/nvim/lua/plugins/zk-nvim.lua
@@ -1,6 +1,26 @@
 return {
 	"zk-org/zk-nvim",
 	config = function()
+		local opts = { noremap = true, silent = false }
+
+		-- Create a new note after asking for its title.
+		vim.api.nvim_set_keymap("n", "<leader>nn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", opts)
+
+		-- Open notes.
+		vim.api.nvim_set_keymap("n", "<leader>no", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", opts)
+		-- Open notes associated with the selected tags.
+		vim.api.nvim_set_keymap("n", "<leader>nt", "<Cmd>ZkTags<CR>", opts)
+
+		-- Search for the notes matching a given query.
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>nf",
+			"<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>",
+			opts
+		)
+		-- Search for the notes matching the current visual selection.
+		vim.api.nvim_set_keymap("v", "<leader>nf", ":'<,'>ZkMatch<CR>", opts)
+
 		require("zk").setup({
 			-- See Setup section below
 			picker = "snacks_picker",
