e695053087adf4069a0691766af2f3715b7cd826
Nick Land <land.d.nicholas@gmail.com>
2025-07-22 12:39:56 -0400

    feat(nvim): clean up lsp config
    * This seems to be how I want lspconfig/blink to work

--- vim/.config/nvim/lua/plugins/lsp.lua
+++ vim/.config/nvim/lua/plugins/lsp.lua
@@ -1,59 +1,33 @@
 return {
 	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile", "VeryLazy" },
+	dependencies = { "saghen/blink.cmp" },
+
+	-- example using `opts` for defining servers
 	opts = {
-		diagnostics = { virtual_text = { prefix = "icons" } },
-	},
-	keys = {
-		{
-			"<leader>ca",
-			function()
-				vim.lsp.buf.code_action()
-			end,
-			desc = "Code Action",
-		},
-		{
-			"<leader>d",
-			function()
-				-- Call the Neovim built-in function to open the diagnostic float
-				vim.diagnostic.open_float(nil, {
-					-- Optional: Customize the float window appearance specifically for this action
-					focusable = false,
-					close_events = { "CursorMoved", "CursorMovedI", "BufHidden", "InsertCharPre", "WinLeave" },
-					border = "rounded",
-				})
-			end,
-			desc = "Show Line Diagnostics (Float)", -- This is what which-key will display
-		},
-	},
-	config = function()
-		if vim.fn.has("nvim-0.11") == 1 and vim.lsp.config then
-			vim.lsp.config("*", {
-				capabilities = require("blink.cmp").get_lsp_capabilities(),
-			})
-		end
-		local configs = require("lspconfig/configs")
-
-		configs.zk = {
-			default_config = {
-				cmd = { "zk", "lsp" },
-				filetypes = { "markdown" },
-				root_dir = function()
-					return vim.loop.cwd()
-				end,
-				settings = {},
-			},
-		}
-
-		vim.diagnostic.config({
-			signs = {
-				text = {
-					[vim.diagnostic.severity.ERROR] = "󰅖 ",
-					[vim.diagnostic.severity.WARN] = " ",
-					[vim.diagnostic.severity.INFO] = " ",
-					[vim.diagnostic.severity.HINT] = "󰌵 ",
+		servers = {
+			lua_ls = {
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = {
+								"vim",
+								"Snacks",
+							},
+						},
+					},
 				},
 			},
-		})
+			ts_ls = {},
+		},
+	},
+
+	config = function(_, opts)
+		local lspconfig = require("lspconfig")
+		for server, config in pairs(opts.servers) do
+			-- passing config.capabilities to blink.cmp merges with the capabilities in your
+			-- `opts[server].capabilities, if you've defined it
+			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+			lspconfig[server].setup(config)
+		end
 	end,
 }
--- vim/.config/nvim/lua/plugins/mason.lua
+++ vim/.config/nvim/lua/plugins/mason.lua
@@ -1,10 +1,32 @@
 return {
 	"mason-org/mason-lspconfig.nvim",
 	opts = {
-		ensure_installed = { "lua_ls" },
+		ensure_installed = {
+			"emmet_ls",
+			"html",
+			"lua_ls",
+			"marksman",
+			"tailwindcss",
+			"ts_ls",
+			"zk",
+		},
 	},
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} },
 		"neovim/nvim-lspconfig",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
+	config = function()
+		local mason_tool_installer = require("mason-tool-installer")
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"eslint_d",
+				"markdownlint",
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"zk",
+			},
+		})
+	end,
 }
