eddbc2ebc9ceb2581dff0a6824eb9efb0fb559b6
Nick Land <land.d.nicholas@gmail.com>
2026-03-05 13:27:58 -0500

    feat(fish): add aliases
    This also exports EDITOR variable.

---
+++ fish/.config/fish/functions/czc.fish
@@ -0,0 +1,3 @@
+function czc --wraps='cz c -l 50' --description 'alias czc=cz c -l 50'
+    cz c -l 50 $argv
+end
---
+++ fish/.config/fish/functions/vale.fish
@@ -0,0 +1,3 @@
+function vale --description "alias vale=vale --config='/Users/tz4m2z/.config/vale/.vale.ini'"
+    command vale --config='/Users/tz4m2z/.config/vale/.vale.ini' $argv
+end
